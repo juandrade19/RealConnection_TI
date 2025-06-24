7d4384a88c96a9088af51c48dfe62924d0249ada
juandrade19 <julialeal1910@gmail.com>
2025-06-24 16:49:06 -0300

    Atualizando hospedagem

--- real-connections/app/login.tsx
+++ real-connections/app/login.tsx
@@ -28,7 +28,7 @@ export default function Login() {
   const endpoint = '/usuarios/login';
 
   try {
-    const response = await fetch(`https://realconnectionpi-production.up.railway.app${endpoint}`, {
+    const response = await fetch(`https://realconnectionti-production.up.railway.app${endpoint}`, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(loginBody),
